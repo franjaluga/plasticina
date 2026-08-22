23d967495773c93625e0ceb5c594dc9f170e0845
franjaluga <proyectosprivado@protonmail.com>
2026-08-22 13:40:33 -0400

    feat/ elimina botón de exportación desde dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,11 +73,6 @@
                 Ver Libro Diario
             </a>
         </div>
-        <div class="space-y-4 mt-4">
-            <a href="{{ route('vc_documents.export_csv') }}" class="text-green-600 hover:underline">
-                Exportar CSV
-            </a>
-        </div>     
     </div>
 
 </body>
